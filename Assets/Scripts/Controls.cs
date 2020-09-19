20effa97c04d6fbcbb9176780a9b8912f4f16980
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2020-09-19 08:16:57 +0200

    Add debug movement via keyboard
    https://pressstart.vip/tutorials/2018/06/22/39/mobile-joystick-in-unity.html

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public float playerSpeed = 2.0f;
+    public Transform playerTransform;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        _movePlayerByKeyboard(new Vector2(Input.GetAxis("Horizontal"),
+                                          Input.GetAxis("Vertical")));
+    }
+
+    [Obsolete("Use this method only for testing and debugging purposes.")]
+    void _movePlayerByKeyboard(Vector2 direction)
+    {
+        playerTransform.Translate(direction * playerSpeed * Time.deltaTime);
+    }
+
+}
