55e423388b45c1d8d1b09d2f5a06c45408194549
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2020-09-20 08:01:08 +0200

    Implement free joystick movement
    https://pressstart.vip/tutorials/2018/06/22/39/mobile-joystick-in-unity.html

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -1,28 +1,94 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Controls : MonoBehaviour
 {
     public float playerSpeed = 2.0f;
-    public Transform playerTransform;
+    public Transform player;
 
-    void Start()
+    bool _screenPressed;
+    Vector2 _pressPoint;
+    Vector2 _dragPoint;
+
+    public Transform innerJoystick;
+    public Transform outerJoystick;
+
+    void FixedUpdate()
     {
-        
+        _controlByJoystick();
+        _controlByKeyboard();
     }
 
     void Update()
     {
-        _movePlayerByKeyboard(new Vector2(Input.GetAxis("Horizontal"),
-                                          Input.GetAxis("Vertical")));
+        _calculateJoystickPosition();
     }
 
-    [Obsolete("Use this method only for testing and debugging purposes.")]
-    void _movePlayerByKeyboard(Vector2 direction)
+    void _calculateJoystickPosition()
     {
-        playerTransform.Translate(direction * playerSpeed * Time.deltaTime);
+        Vector3 worldPressPoint = Camera.main.ScreenToWorldPoint(
+            new Vector3(Input.mousePosition.x, Input.mousePosition.y,
+                        Camera.main.transform.position.z));
+
+        // Button is clicked.
+        if (Input.GetMouseButtonDown(0))
+        {
+            innerJoystick.transform.position =
+                outerJoystick.transform.position = _pressPoint = worldPressPoint;
+
+            _toggleJoystickVisibility(true);
+        }
+
+        // Button is held.
+        if (Input.GetMouseButton(0))
+        {
+            _dragPoint = worldPressPoint;
+           _screenPressed = true;
+        }
+        else
+        {
+           _screenPressed = false;
+        }
     }
 
+    void _controlByJoystick()
+    {
+        Vector2 direction;
+
+        if (_screenPressed)
+        {
+            direction = Vector2.ClampMagnitude(_dragPoint - _pressPoint, 1.0f);
+            player.transform.Translate(direction * playerSpeed
+                                       * Time.deltaTime);        
+
+            innerJoystick.transform.position =
+                new Vector2(_pressPoint.x + direction.x,
+                            _pressPoint.y + direction.y);
+        }
+        else
+        {
+            _toggleJoystickVisibility(false);
+        }
+    }
+
+    [Obsolete("Use only for debugging/testing purposes.")]
+    void _controlByKeyboard()
+    {
+        if (!_screenPressed)
+        {
+            _movePlayer(new Vector2(Input.GetAxis("Horizontal"),
+                                    Input.GetAxis("Vertical")));
+        }
+    }
+
+    void _movePlayer(Vector2 direction)
+    {
+        player.transform.Translate(direction * playerSpeed * Time.deltaTime);    
+    }
+
+    void _toggleJoystickVisibility(bool visible)
+    {
+        innerJoystick.GetComponent<SpriteRenderer>().enabled =
+            outerJoystick.GetComponent<SpriteRenderer>().enabled = visible;
+    }
 }
