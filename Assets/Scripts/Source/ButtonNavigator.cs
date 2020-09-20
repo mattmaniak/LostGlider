0342ceba87a71fd0b2fce63ca836e15c512a9ae6
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2020-09-20 11:03:10 +0200

    Unseal ButtonNavigator class as it's not necessary

--- Assets/Scripts/Source/ButtonNavigator.cs
+++ Assets/Scripts/Source/ButtonNavigator.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public sealed class ButtonNavigator : MonoBehaviour
+public class ButtonNavigator : MonoBehaviour
 {
     // Load, switch a next and unload a previous scene.
     public void SwitchToScene(string sceneName)
