a230fb7c00fdeb632dad628126e4dd523c170ce7
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2020-09-20 10:51:47 +0200

    Add very basic menu
    https://www.loekvandenouweland.com/content/use-unity-button-to-change-between-scenes.html

---
+++ Assets/Scripts/Source/ButtonNavigator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public sealed class ButtonNavigator : MonoBehaviour
+{
+    // Load, switch a next and unload a previous scene.
+    public void SwitchToScene(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+}
