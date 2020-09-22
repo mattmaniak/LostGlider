a4707a8e4f78479c4359be5ec2b15579d5a2a605
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2020-09-22 06:46:21 +0200

    Rename ButtonNavigator to SceneNavigator as it navigates throught scenes

--- Assets/Scripts/Source/SceneNavigator.cs
+++ Assets/Scripts/Source/SceneNavigator.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ButtonNavigator : MonoBehaviour
+public class SceneNavigator : MonoBehaviour
 {
     // Load, switch a next and unload a previous scene.
     public void SwitchToScene(string sceneName)
