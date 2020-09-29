a9b882234c2777fcf11a6420195aba100ed25fd0
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2020-09-29 11:55:14 +0200

    Add required components to player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Player : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D collision)
