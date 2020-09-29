8986b195a4e302ec42f3af211c587e31549639b6
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2020-09-29 09:50:04 +0200

    Load ground sprites

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,39 @@
+﻿#undef DEBUG
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+    List<Sprite> groundsPool = new List<Sprite>();
+
+    void Start()
+    {
+        const int filesNumber = 2;
+        Sprite loadedSprite;
+        string spriteFilename;
+
+        for (int i = 0; i < filesNumber; i++)
+        {
+            spriteFilename = "Sprites/Ground" + i + ".psd";
+            loadedSprite = Resources.Load<Sprite>(spriteFilename);
+
+            if (loadedSprite == null)
+            {
+#if DEBUG
+                Debug.Log(GetType().Name +
+                          " initialization aborted. Unable to load: "
+                          + spriteFilename);
+#endif
+                return;
+            }
+            groundsPool.Add(loadedSprite);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
