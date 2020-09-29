e8de1563974ec16540cc3686feac31e82d33df24
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2020-09-29 11:15:32 +0200

    Load and place prefab instances properly in the world
    Fix also ground sprites dir in Resources/ folder.

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    [SerializeField]
+    GameObject groundPrefab;
 
-    List<Sprite> groundsPool = new List<Sprite>();
+    List<GameObject> groundsPool = new List<GameObject>();
 
     void Start()
     {
-        const int filesNumber = 2;
+        const int spritesNumber = 2;
         Sprite loadedSprite;
         string spriteFilename;
 
-        for (int i = 0; i < filesNumber; i++)
+        for (int i = 0; i < spritesNumber; i++)
         {
-            spriteFilename = "Sprites/ground" + i + ".psd";
+            spriteFilename = "Sprites/Level/Grounds/ground" + i;
             loadedSprite = Resources.Load<Sprite>(spriteFilename);
 
             if (loadedSprite == null)
@@ -28,12 +30,10 @@ public class LevelGenerator : MonoBehaviour
 #endif
                 return;
             }
-            groundsPool.Add(loadedSprite);
+            groundsPool.Add(Instantiate(groundPrefab));
+            groundsPool[i].GetComponent<SpriteRenderer>().sprite = loadedSprite;
         }
-    }
-
-    void Update()
-    {
-        
+        groundsPool[0].transform.Translate(Vector3.right * 2.0f) ;
+        groundsPool[1].transform.Translate(Vector3.left * 2.0f) ;
     }
 }
