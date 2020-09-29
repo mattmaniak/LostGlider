b381bf4df9280b7a3b953561526b5785c06a3891
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2020-09-29 11:50:28 +0200

    Update BoxColliders2D of GroundPrefab instances

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -14,26 +14,34 @@ public class LevelGenerator : MonoBehaviour
     {
         const int spritesNumber = 2;
         Sprite loadedSprite;
-        string spriteFilename;
+        string spriteBasename;
 
         for (int i = 0; i < spritesNumber; i++)
         {
-            spriteFilename = "Sprites/Level/Grounds/ground" + i;
-            loadedSprite = Resources.Load<Sprite>(spriteFilename);
+            spriteBasename = "Sprites/Level/Grounds/ground" + i;
+            loadedSprite = Resources.Load<Sprite>(spriteBasename);
 
             if (loadedSprite == null)
             {
 #if DEBUG
                 Debug.Log(GetType().Name +
                           " initialization aborted. Unable to load: "
-                          + spriteFilename);
+                          + spriteBasename);
 #endif
                 return;
             }
             groundsPool.Add(Instantiate(groundPrefab));
             groundsPool[i].GetComponent<SpriteRenderer>().sprite = loadedSprite;
+
+            groundsPool[i].GetComponent<BoxCollider2D>().offset
+                = groundsPool[i].GetComponent<SpriteRenderer>().
+                  sprite.bounds.center;
+            
+            groundsPool[i].GetComponent<BoxCollider2D>().size
+                = groundsPool[i].GetComponent<SpriteRenderer>().
+                  sprite.bounds.size;
         }
+        // TODO: TEMPONARY POSITION.
         groundsPool[0].transform.Translate(Vector3.right * 2.0f) ;
-        groundsPool[1].transform.Translate(Vector3.left * 2.0f) ;
     }
 }
